c7bdcc2441473b43c264cc26b5a59bfed72990d1
Plato Balomenakis <pbalomenakis@zipcar.co.uk>
2014-02-25 18:28:41 +0000

    added partial-mock examples

--- java/src/test/java/com/example/ServerEasymockTest.java
+++ java/src/test/java/com/example/ServerEasymockTest.java
@@ -8,20 +8,24 @@ import static org.easymock.EasyMock.*;
 public class ServerEasymockTest {
 
     /*
-     With easymock we create mocks using the createMock(..) method, and then we stub or mock it's methods.
+        With easymock we create mocks using the createMock(..) method, and then we stub or mock it's methods.
+        We can create three types of mock objects:
+            default : throws assertion errors for any unexpected invocations
+            nice : allows unexpected invocations by returning null, false, 0
+            strict : similar to default with order checking enabled
      */
 
 
     @Test
     public void passes_shouldLogAction() {
 
-        Preferences preferencesStub = createMock(Preferences.class);
         // use the stub to force the sut to use the logger (note the stubReturn)
+        Preferences preferencesStub = createMock(Preferences.class);
         expect(preferencesStub.isLoggingEnabled()).andStubReturn(true);
         replay(preferencesStub);
 
-        Logger loggerMock = createNiceMock(Logger.class);
         // set expectations on the logger
+        Logger loggerMock = createNiceMock(Logger.class);
         loggerMock.log("did something"); // expectation with specific argument
         replay(loggerMock);
 
@@ -34,6 +38,7 @@ public class ServerEasymockTest {
         verify(loggerMock);
     }
 
+
     // A strict mock for easymock is a mock, which expects the calls in a specific order.
     @Test
     public void fails_order_of_expectations() {
@@ -42,7 +47,7 @@ public class ServerEasymockTest {
         replay(preferencesStub);
 
         Logger loggerMock = createStrictMock(Logger.class);
-        loggerMock.saveLogs(); // fails because the logger.saveLogs() is be called after logger.log(..)
+        loggerMock.saveLogs(); // fails because logger.saveLogs() is called after logger.log(..)
         loggerMock.log("did something");
         replay(loggerMock);
 
@@ -55,6 +60,31 @@ public class ServerEasymockTest {
         verify(loggerMock);
     }
 
+
+    // Partial mocking
+
+    @Test
+    public void passes_shouldLogActionToo() {
+
+        Preferences preferencesStub = createMock(Preferences.class);
+        expect(preferencesStub.isLoggingEnabled()).andStubReturn(true);
+        replay(preferencesStub);
+
+        // create the partial mock, using an implementation class
+        Logger loggerMock = createMockBuilder(LoggerImpl.class)
+                .addMockedMethod("log", String.class)
+                .createMock();
+        loggerMock.log("did something"); // set expectation
+        replay(loggerMock);
+
+        Server sut = new Server(loggerMock, preferencesStub);
+
+        // act
+        sut.doSomething();
+
+        // assert
+        verify(loggerMock);
+    }
 
 
 }
